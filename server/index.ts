101541fd9bf460a1c9308fa741248ef8f84cba5b
Manu <manoelaps2022@gmail.com>
2025-09-16 22:20:28 -0300

    tentando evitar conflitos

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,6 @@ import couponRoutes from './routes/CouponRoutes';
 import orderRoutes from './routes/OrderRoutes';
 import cartRoutes from './routes/CartRoutes';
 import adminRoutes from './routes/AdminRoutes';
-import shippingRoutes from './routes/ShippingRoutes'; // IMPORTAR A NOVA ROTA
 
 const app = express();
 
@@ -35,6 +34,7 @@ app.use('/api', productRoutes);
 app.use('/api', couponRoutes);
 app.use('/api', orderRoutes);
 app.use('/api', cartRoutes);
+app.use('/api', adminRoutes); USAR A NOVA ROTA
 
 app.use(express.static('public'));
 
